7faa9b4007ce1068a356c7ddfe1469d4752e3d2b
Samara Melo <samara.melo299@gmail.com>
2024-01-12 09:34:45 -0300

    atualização

---
+++ exercicios/ex08/ex08.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 50) { // Condição Simples
+console.log('Você ultrapassou a velocidade permitida. MULTADO!') 
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
