fcd5a8787264f09f5cb85a62ffdf0bdf45cbe185
Samara Melo <samara.melo299@gmail.com>
2024-01-13 15:24:17 -0300

    atualização

---
+++ exercicios/ex14/ambiente.js
@@ -0,0 +1,17 @@
+// DO WHILE
+
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++; // c = c + 1;
+} while (c <= 10)
+
+
+/* WHILE
+
+var c = 1;
+while (c <= 10) {
+    console.log(`Passo ${c}`);
+    c++; // c = c + 1;
+}
+*/
---
+++ exercicios/ex15/ambiente.js
@@ -0,0 +1,23 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+/* FOR
+
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+*/
+
+/* WHILE
+
+var c = 1;
+while (c <= 10) {
+    console.log(`Passo ${c}`);
+    c++; // c = c + 1;
+}
+*/
+
