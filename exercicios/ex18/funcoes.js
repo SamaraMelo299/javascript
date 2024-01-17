0a9219695cf372ced07e5a8d8fb9cbd5a05f84c9
Samara Melo <samara.melo299@gmail.com>
2024-01-17 14:30:11 -0300

    atualização

---
+++ exercicios/ex18/funcoes.js
@@ -0,0 +1,5 @@
+function saudacao (nome) {
+    return "Olá, " + nome + "!";
+}
+let mensagem = saudacao ("Samara Dev");
+console.log(mensagem);
---
+++ exercicios/ex19/funcao02.js
@@ -0,0 +1,6 @@
+// Parametro opcional (nx=0) para que quando haja somente um valor a ser somado, ele não dê Nan!
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma (2))
